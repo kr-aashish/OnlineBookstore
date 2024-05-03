09620e47770e92462d6b89607a21a1f1e5cf56bb
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2024-05-03 22:20:49 +0530

    Add initialise DB Script

--- initialiseDatabase.sql
+++ initialiseDatabase.sql
@@ -1,10 +1,21 @@
+DROP TABLE BOOKS CASCADE;
+DROP TABLE CATEGORIES CASCADE;
+
 -- CREATE TABLE categories (
 --   category_id INT AUTO_INCREMENT PRIMARY KEY,
 --   name VARCHAR(50) NOT NULL
 -- );
 
+-- CREATE TABLE categories (
+--   -- category_id SERIAL PRIMARY KEY,
+--   category_id INT PRIMARY KEY,
+--   name TEXT NOT NULL
+-- );
+
+CREATE SEQUENCE category_id_seq START 1;
+
 CREATE TABLE categories (
-  category_id SERIAL PRIMARY KEY,
+  category_id INT DEFAULT nextval('category_id_seq') PRIMARY KEY,
   name TEXT NOT NULL
 );
 
@@ -17,8 +28,20 @@ CREATE TABLE categories (
 --   category_id INT NOT NULL,
 --   CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES categories(category_id)
 -- );
+-- CREATE TABLE books (
+--   -- book_id SERIAL PRIMARY KEY,
+--   book_id INT PRIMARY KEY,
+--   title TEXT NOT NULL,
+--   author_name TEXT NOT NULL,
+--   price DECIMAL(10, 2) NOT NULL,
+--   category_id INT NOT NULL,
+--   CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES categories(category_id)
+-- );
+
+CREATE SEQUENCE book_id_seq START 1;
+
 CREATE TABLE books (
-  book_id SERIAL PRIMARY KEY,
+  book_id INT DEFAULT nextval('book_id_seq') PRIMARY KEY,
   title TEXT NOT NULL,
   author_name TEXT NOT NULL,
   price DECIMAL(10, 2) NOT NULL,
